17b84bfc7c48655357f403bfadcf23ae77fb6b6c
WeryBeardedMan <136243862+WeryBeardedMan@users.noreply.github.com>
2024-03-02 22:20:59 +0300

    Delete HW_12/planetasad directory

--- HW_12/planetasad/config.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-# Настройка опций для headless режима
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
-base_url = 'https://www.planetsad.ru/'
